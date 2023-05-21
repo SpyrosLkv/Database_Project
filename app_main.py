c3ce08d994a7672fecc0b471eb1ef668e1c33c47
Spyros Loukovitis <spyrosloukovitis@gmail.com>
2023-05-21 10:50:33 -0700

    phone in the registration

--- app_main.py
+++ app_main.py
@@ -63,6 +63,7 @@ def signUp():
         user_role = request.form['inputUser_Role']
         # library_id = int(request.form['inputLibrary'])
         library_name = request.form.get("dropdown")
+        phoneno = int(request.form['inputPhone'])
 
         if first_name and last_name and _email and username and _password:
             with mysql.connection.cursor() as cursor:
@@ -83,6 +84,11 @@ def signUp():
                     query = "insert into Pending_Registrations (username,password_hashed,first_name,last_name,birth_date,email,user_role,library_id) values (%s,%s,%s,%s,%s,%s,%s,"+str(library_id)+");"
                     params = (username,HashPass(_password),first_name,last_name,birth_date,_email,user_role)
                     cursor.execute(query,params)
+                    mysql.connection.commit()
+                    print("hello")
+                    query = "insert into Reg_Phone_No (number,registration_username) VALUES ("+str(phoneno)+",%s);"
+                    params = (username,)
+                    cursor.execute(query,params,)
                     mysql.connection.commit()
 
                     return json.dumps({'message': 'User created successfully !', 'redirect_url': '/'})
@@ -279,16 +285,29 @@ def process_registration():
         print(username, action)
         with mysql.connection.cursor() as cursor:
             if action == "deny":
+                query = "DELETE FROM Reg_Phone_No WHERE registration_username = %s;"
+                params = (username,)
+                cursor.execute(query,params)
+                mysql.connection.commit()
                 query = "DELETE FROM Pending_Registrations WHERE username = '"+username+"';"
                 cursor.execute(query)
                 mysql.connection.commit()
                 return json.dumps({'redirect_url': '/pending_registrations'})
             elif action == "accept":
+                query = "SELECT * FROM Reg_Phone_No WHERE registration_username = %s;"
+                params = (username,)
+                cursor.execute(query,params)
+                phones = cursor.fetchall()
+                query = "DELETE FROM Reg_Phone_No WHERE registration_username = %s;"
+                params = (username,)
+                cursor.execute(query,params)
+                mysql.connection.commit()
+
                 query = "SELECT * FROM Pending_Registrations WHERE username = '"+username+"';"
                 cursor.execute(query)
                 data = cursor.fetchall()
+                library_id = int(data[0][7])
                 data = data[0]
-                library_id = int(data[7])
                 query = "DELETE FROM Pending_Registrations WHERE username = '"+username+"';"
                 cursor.execute(query)
                 mysql.connection.commit()
@@ -297,7 +316,7 @@ def process_registration():
                 print(query, params)
                 cursor.execute(query,params)
                 mysql.connection.commit()
-                ''' 
+                '''
                 Create User's first Card
                 '''
                 query = "SELECT user_id from Users where username = %s;"
@@ -306,8 +325,19 @@ def process_registration():
                 user_id = int(cursor.fetchall()[0][0])
 
                 query = "INSERT INTO Card (user_id,card_no,status) VALUES ("+str(user_id)+",1,'Active');"
-                cursor.ececute(query)
+                cursor.execute(query)
                 mysql.connection.commit()
+
+
+                '''
+                Input Phone Numbers
+                '''
+                for phone in phones:
+                    phone = int(phone[0])
+                    query = "INSERT INTO User_Phone_No (number,user_id) VALUES ("+str(phone)+","+str(user_id)+");"
+                    cursor.execute(query)
+                    mysql.connection.commit()
+
                 return json.dumps({'redirect_url': '/pending_registrations'})
             else:
                 return json.dumps({'error': 'Something has gone wrong!'})
